beb7b5caae83ee35cf66e5324ab0c117fc1c2509
Leo Diaz <leo@crazyapp.cloud>
2020-07-23 00:03:18 +0800

    cleanup

--- api.py
+++ api.py
@@ -98,30 +98,3 @@ api.add_resource(UserAuthResource, '/user/auth', )
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-"""
-DESIGN DOC
-
-General Overview
-
-PHASE 1:
-- Create a web application for sharing homemade cooked meals.
-- As a user, I can create an account to the site, then make my own recipe.
-- As a user, I can publish my own recipe or make it private
-- As a user, my recipe can be peer reviewed and rated by other users
-- As a user, I can review or bookmark someone's public recipe
-- As a user, I can search a recipe using my available ingredients
-
-PHASE 2:
-- As as user, I
-
-Login and Registration
-
-
-cookeryuser / P@ssw0rd!
-
-TODO LATER:
-https://www.youtube.com/watch?v=kRNXKzfYrPU
-https://flask-marshmallow.readthedocs.io/en/latest/
-
-"""

314ae5bb266b8a2f8fd392541867c195004ef806
Leo Diaz <leo@crazyapp.cloud>
2020-08-08 02:23:28 +0800

    code-cleanup

--- resources/dishes.py
+++ resources/dishes.py
@@ -8,43 +8,51 @@ from common.serializer import *
 from common.schema import DishSchema
 
 class DishesResource(Resource):
+    @jwt_required
     def get(self):
+        if not get_jwt_identity(): return ErrorSerializer().dump(dict(message=UNAUTHORIZED_ERROR, errors=['Invalid token. Please try again'])), 401
+
         req = request.args
         errors = PaginationQSValidator().validate(req)
 
-        if errors:
-            return ErrorSerializer().dump(dict(message=BAD_REQUEST, errors=errors)), 400
-        else:
-            dish = Dish.get_all_dishes(**req)
-            list = {
-                "list": DishSchema(many=True).dump(dish.items),
-                "count": len(dish.items),
-                "total": dish.total
-            }
+        if errors: return ErrorSerializer().dump(dict(message=BAD_REQUEST, errors=errors)), 400
 
-            return SuccessSerializer().dump(
-                dict(message="OK", data=list)
-            ), 200
+        dish = Dish.get_all_dishes(**req)
+        list = {
+            "list": DishSchema(many=True).dump(dish.items),
+            "count": len(dish.items),
+            "total": dish.total
+        }
+
+        return SuccessSerializer().dump(
+            dict(message="OK", data=list)
+        ), 200
 
 class DishResource(Resource):
     @jwt_required
     def patch(self, dish_id):
         if not get_jwt_identity(): return ErrorSerializer().dump(dict(message=UNAUTHORIZED_ERROR, errors=['Invalid token. Please try again'])), 401
-        else:
-            req = request.get_json(force=True)
-            print(req)
-            errors = DishUpdateSerializer().validate(req)
 
-            if errors: return ErrorSerializer().dump(dict(message=BAD_REQUEST, errors=errors)), 400
-            else:
-                return "asda"
+        req = request.get_json(force=True)
+        errors = DishUpdateSerializer().validate(req)
+
+        if errors: return ErrorSerializer().dump(dict(message=BAD_REQUEST, errors=errors)), 400
+
+        data = Dish.find_by_id(dish_id)
+        return SuccessSerializer().dump(
+            dict(message="OK", data=DishSchema().dump(data))
+        ), 200
 
     @jwt_required
     def get(self, dish_id):
         if not get_jwt_identity(): return ErrorSerializer().dump(dict(message=UNAUTHORIZED_ERROR, errors=['Invalid token. Please try again'])), 401
-        else:
-            data = Dish.find_by_id(dish_id)
 
-            return SuccessSerializer().dump(
-                dict(message="OK", data=DishSchema().dump(data))
-            ), 200
+        data = Dish.find_by_id(dish_id)
+
+        return SuccessSerializer().dump(
+            dict(message="OK", data=DishSchema().dump(data))
+        ), 200
+
+class PrepInstructionResource(Resource):
+    def patch(self):
+        pass

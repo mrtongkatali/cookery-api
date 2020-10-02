d8401c02ab66e78a375a2a7c3b5abc3bfb1d8f93
Leo Diaz <leo@crazyapp.cloud>
2020-10-02 23:55:20 +0800

    change url convention

--- routes.py
+++ routes.py
@@ -8,16 +8,16 @@ def initialize_routes(api):
     api.add_resource(UserAuthResource, '/v1/user/auth',)
     api.add_resource(DishesResource,'/v1/dishes',)
     api.add_resource(DishResource,
-        '/v1/dish/new',
-        '/v1/dish/update/<int:dish_id>',
-        '/v1/dish/<int:dish_id>',
+        '/v1/dishes/new',
+        '/v1/dishes/update/<int:dish_id>',
+        '/v1/dishes/<int:dish_id>',
     )
     api.add_resource(IngredientResource,
-        '/v1/ingredient/new',
-        '/v1/ingredient/update/<int:ingr_id>',
-        '/v1/ingredient/<int:ingr_id>',
+        '/v1/ingredients/new',
+        '/v1/ingredients/update/<int:ingr_id>',
+        '/v1/ingredients/<int:ingr_id>',
     )
-    api.add_resource(DishImport, '/v1/dish/import',)
+    api.add_resource(DishImport, '/v1/dishes/import',)
 
     # api.add_resource(TodoSimple,
     # #     '/todo/<string:todo_id>',
